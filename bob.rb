35d4bcee9250416b29d24dec1b8f04682b9c6678
Cris Joya <joya_cris@outlook.com>
2016-06-08 14:03:55 -0500

    Update bob.rb

--- bob.rb
+++ bob.rb
@@ -1,1 +1,14 @@
+class Bob
+  def hey(remark)
+    if remark.strip.empty?
+      'Fine. Be that way!'
+    elsif remark.upcase == remark && remark != remark.downcase
+      'Whoa, chill out!'
+    elsif remark.end_with?('?')
+      'Sure.'
+    else
+      'Whatever.'
+    end
+  end
+end
 
